1c96f5df8cae566a70d523b101811cc979203f1e
Ben Fute <https://benfute@github.com>
2023-07-26 19:58:57 -0500

    Initial Commit

---
+++ NeuralNetwork/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  NeuralNetwork
+//
+//  Created by Ben Fute on 7/26/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
