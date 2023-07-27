21f75ffb35d98e60313fe618dfb7715984401213
Ben Fute <https://benfute@github.com>
2023-07-26 20:02:48 -0500

    Added Headers and constants

--- NeuralNetwork/main.c
+++ NeuralNetwork/main.c
@@ -6,6 +6,12 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define INPUT_SIZE 2
+#define HIDDEN_SIZE 3
+#define OUTPUT_SIZE 1
 
 int main(int argc, const char * argv[]) {
     // insert code here...
